9690707ab4c098a3e3729996ad66908418e7c2b7
Martin Larsson <martin.larsson@math.lth.se>
2020-12-03 22:19:30 +0100

    Improved performance by 100 %
    Optimized pruning code resulting in a halving of the execution time.

--- prunelaman.h
+++ prunelaman.h
@@ -3,9 +3,13 @@
 
 #define EMBED_DIM 2
 #define EMBED_DOF (EMBED_DIM * (EMBED_DIM + 1) / 2)
-#define HIGHEST_BIT(type) (~((type)-1 >> 1))
+#define FIRST_NODE (~((graph)-1 >> 1))
+#define FIRST_NODES(n) (~((graph)-1 >> (n)))
+#define FIRST_TWO_NODES FIRST_NODES(2)
+#define NTH_NODE(n) (FIRST_NODE >> (n))
 
 #define PRUNE prunelaman
+// #define OUTPROC countgraphs
 
 /* Note: PLUGIN_INIT happens after validation of the input arguments in geng.c.
  * Beware of illegal argument combinations. */
@@ -18,6 +22,20 @@
     {                                                                       \
         geng_mindeg = mindeg = EMBED_DIM;                                   \
     }
+
+static nauty_counter total_number_of_graphs = 0;
+
+void countgraphs(FILE *f, graph *g, int n)
+{
+    ++total_number_of_graphs;
+    /* report on every 2^30 ≈ 10^9 graph ≈ every hour */
+    if ((total_number_of_graphs & (1 << 30) - 1) == 0)
+    {
+        fprintf(stderr, ">A " COUNTER_FMT " graphs generated\n",
+                total_number_of_graphs);
+        fflush(stderr);
+    }
+}
 
 int prunelaman(graph *g, int n, int maxn)
 {
@@ -52,24 +70,20 @@ int prunelaman(graph *g, int n, int maxn)
     for (k = n - 1; k > EMBED_DIM + 1; --k)
     {
         /* init subgraph with the k-1 first nodes and the new node */
+        mask = FIRST_NODES(k - 1) | NTH_NODE(n - 1);
+        l = 0;
         for (i = 0; i < k - 1; ++i)
+        {
             nodeinds[i] = i;
+            l += POPCOUNT(g[i] & mask);
+        }
         nodeinds[k - 1] = n - 1;
+        l += POPCOUNT(g[n - 1] & mask);
+        l = l / 2;
 
         subgraphsleft = TRUE;
         while (subgraphsleft)
         {
-            /* mask out all nodes not in the subgraph */
-            mask = 0;
-            for (i = 0; i < k; ++i)
-                mask |= HIGHEST_BIT(graph) >> nodeinds[i];
-
-            /* find number of edges in subgraph */
-            l = 0;
-            for (i = 0; i < k; ++i)
-                l += POPCOUNT(g[nodeinds[i]] & mask);
-            l = l / 2;
-
             /* subgraph is overdetermined => not minimal */
             if (l > EMBED_DIM * k - EMBED_DOF)
                 return TRUE;
@@ -77,18 +91,28 @@ int prunelaman(graph *g, int n, int maxn)
             /* go to next subgraph */
             for (i = k - 2; i >= 0; --i)
             {
-                nodeinds[i]++;
-                if (nodeinds[i] >= n + i - k + 1)
+                if (nodeinds[i] < n + i - k)
+                {
+                    l -= POPCOUNT(g[nodeinds[i]] & mask);
+                    mask ^= FIRST_TWO_NODES >> nodeinds[i];
+                    ++nodeinds[i];
+                    l += POPCOUNT(g[nodeinds[i]] & mask);
+
+                    for (j = i + 1; j < k - 1; ++j)
+                    {
+                        l -= POPCOUNT(g[nodeinds[j]] & mask);
+                        mask &= ~NTH_NODE(nodeinds[j]);
+                        nodeinds[j] = nodeinds[i] + j - i;
+                        mask |= NTH_NODE(nodeinds[j]);
+                        l += POPCOUNT(g[nodeinds[j]] & mask);
+                    }
+                    break;
+                }
+                else
                 {
                     /* wrap, continue and increase next index */
                     if (i == 0)
                         subgraphsleft = FALSE;
-                }
-                else
-                {
-                    for (j = i + 1; j < k - 1; ++j)
-                        nodeinds[j] = nodeinds[i] - i + j;
-                    break;
                 }
             }
         }
